ce4ee74262e1c9ba36316fff8d6c7621ac21a344
Emil Cazamir <emil.cazamir@arabesque.ro>
2021-06-23 13:57:50 +0300

    Fix - cheie SSH user local

--- infra/cloud/z-metadata-startup-script.sh
+++ infra/cloud/z-metadata-startup-script.sh
@@ -10,7 +10,7 @@ sudo ansible-galaxy collection install community.general
 # Creare cheie SSH locala
 su - ubuntu -c 'SSH_KEY=~/.ssh/id_rsa; umask 077; if [ ! -d ~/.ssh -o ! -f ${SSH_KEY} ]; then ssh-keygen -b 2048 -t rsa -f ${SSH_KEY} -q -N ""; cat ${SSH_KEY}.pub > ~/.ssh/authorized_keys ; fi'
 # Adauga cheie SSH pentru contul administrativ
-su - ubuntu -c "echo 'ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNJ/uojfTXvAo3OwZHy0pWPXKx0pXwgH+7EZ25JP8KUqQB4SzXaFLsPhxuDka/EZ3W1BwbByOSwi5cygPZ2Y3l8= ecdsa 256-051621' > ~/.ssh/authorized_keys"
+su - ubuntu -c "echo 'ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNJ/uojfTXvAo3OwZHy0pWPXKx0pXwgH+7EZ25JP8KUqQB4SzXaFLsPhxuDka/EZ3W1BwbByOSwi5cygPZ2Y3l8= ecdsa 256-051621' >> ~/.ssh/authorized_keys"
 # Copie cheia gazdei in lista de chei cunoscute 
 su - ubuntu -c 'ssh-keyscan -t ecdsa localhost 2>/dev/null 1>~/.ssh/known_hosts'
 sudo mkdir /app; chown ubuntu:ubuntu /app
