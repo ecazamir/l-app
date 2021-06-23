33e1293b5f5c02bdaff3d4e9f2efe7743ef9810c
Emil Cazamir <emil.cazamir@arabesque.ro>
2021-06-23 15:16:45 +0300

    Test servlet version 3.1 (Tomcat v. 8) - fail IT

--- src/main/java/constants/Vars.java
+++ src/main/java/constants/Vars.java
@@ -5,7 +5,7 @@
 package constants;
 
 public class Vars {
-	public final static String expectedServletApiString = "3.0";
+	public final static String expectedServletApiString = "3.1";
 	public final static int cargoListenerPort = 8080; 
 	/* Containerul Crago este invocat de Maven. Numărul de port trebuie să corespundă
 	 * cu numărul din ./pom.xml */
